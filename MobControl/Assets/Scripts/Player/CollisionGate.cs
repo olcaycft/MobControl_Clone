c12fb719a8b7b2a5d1a3aca3f7f4789a790c5d9b
Olcay ÇİFTÇİ <olcay_farmer@hotmail.com>
2022-01-07 18:45:02 +0300

    Add: Different x gates

--- MobControl/Assets/Scripts/Player/CollisionGate.cs
+++ MobControl/Assets/Scripts/Player/CollisionGate.cs
@@ -7,20 +7,56 @@ using Random = UnityEngine.Random;
 public class CollisionGate : MonoBehaviour
 {
     private float arrangeX;
+    private float zChange = 0.5f;
+    private float minRange;
+    private float maxRange;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("2xGate"))
         {
-            gameObject.SetActive(false);
-            for (int i = 0; i < 2; i++)
-            {
-                arrangeX = Random.Range(-0.8f, +0.8f);
-                var pos = transform.position;
-                pos.x += arrangeX;
-                pos.z += 0.5f;
-                ObjectPooler.Instance.SpawnFromPool("Player", pos, transform.rotation);
-            }
+            SetActiveAfterGate(2);
         }
+        else if (other.gameObject.CompareTag("3xGate"))
+        {
+            SetActiveAfterGate(3);
+        }
+        else if (other.gameObject.CompareTag("4xGate"))
+        {
+            SetActiveAfterGate(4);
+        }
+    }
+
+
+    private void SetActiveAfterGate(int count)
+    {
+        gameObject.SetActive(false);
+        if (count==2)
+        {
+            minRange = -0.8f;
+            maxRange = 0.8f;
+        }
+        else if (count==3)
+        {
+            minRange = -1f;
+            maxRange = 1f;
+        }
+        else if (count==4)
+        {
+            minRange = -1.3f;
+            maxRange = 1.3f;
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            zChange += 0.1f;
+            arrangeX = Random.Range(minRange, maxRange);
+            var pos = transform.position;
+            pos.x += arrangeX;
+            pos.z += zChange;
+            ObjectPooler.Instance.SpawnFromPool("Player", pos, transform.rotation);
+        }
+
+        zChange = 0.1f;
     }
 }
